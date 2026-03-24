6ca0c776fe1a233ead92429dfb8c7aa1b833ef6e
Bhavna Seshadri <bhavses@google.com>
2026-03-24 11:25:18 -0400

    Initial commit of agent source

---
+++ test/my_agent.py
@@ -0,0 +1,14 @@
+class MyAgent:
+
+  def query_none(self):
+    return None
+
+  def query_list(self):
+    return [1, 2, 3]
+
+  def register_operations(self):
+    return {
+        "": ["query_none", "query_list"],
+    }
+
+agent = MyAgent()
